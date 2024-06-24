9dcb94c97bc6d91f6cb5627893a641f1fb2a95cc
Francesco Galvao <francescogalvao@gmail.com>
2024-06-23 21:24:03 -0300

    Correct folder tree and test classes names

--- simulator/tests/unit/test_bubble_sort.py
+++ simulator/tests/unit/test_bubble_sort.py
@@ -6,7 +6,7 @@ import unittest
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 sys.path.append(project_root)
 
-from simulator.apps.bubble_sort import bubble_sort
+from simulator.src.bubble_sort import bubble_sort
 
 
 class TestBubbleSort(unittest.TestCase):
